e428071935944889b926512a17c2679ac69e1623
Sunflower64 <estebancortesdonoso@gmail.com>
2020-09-12 17:54:22 -0300

    Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="navbar">
+        <header>
+            <img src="" alt="logo-colegio">
+            <h2>Colegio Nombre</h2>
+        </header>
+        <div class="navlinks">
+            <ul>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+            </ul>
+        </div>
+    </div>
+</template>
+<style lang="sass">
+    
+</style>
