9d51ad2f837c48388ee20602d7285f8aaf2b69a8
Sunflower64 <estebancortesdonoso@gmail.com>
2020-09-12 21:35:54 -0300

    NavBar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,15 +6,15 @@
         </header>
         <div class="navlinks">
             <ul>
+                <li><a href="">Material</a></li>
+                <li><a href="">Funcionarios</a></li>
                 <li><a href=""></a></li>
                 <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
+                <li><a href="">Multimedia</a></li>
             </ul>
         </div>
     </div>
 </template>
-<style lang="css">
+<style scoped>
     
 </style>
